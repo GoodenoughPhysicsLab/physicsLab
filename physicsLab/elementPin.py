f4209ad419adf0fe272a0d0718943003dbd27abb
Goodenough <11533108+script2000@user.noreply.gitee.com>
2023-02-11 11:03:28 +0000

    删除文件 physicsLab

--- physicsLab/elementPin.py
+++
@@ -1,8 +0,0 @@
-# Òý½ÅÀà
-class element_Pin:
-    def __init__(self, input_self,  pinLabel : int):
-        self.element_self = input_self
-        self.pinLabel = pinLabel
-
-    def type(self) -> str:
-        return 'element Pin'
--- physicsLab/packageTool.py
+++
@@ -1,6 +0,0 @@
-from typing import Union
-
-def myRound(num : Union[int, float]):
-    if isinstance(num, int):
-        return float(num)
-    return round(num, 4)
