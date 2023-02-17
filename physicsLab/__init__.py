01a4a48b51f7852c44b496b8c53d510e395fc804
Goodenough <11533108+script2000@user.noreply.gitee.com>
2023-02-17 11:35:46 +0000

    physicsLab __init__
    Signed-off-by: Goodenough <11533108+script2000@user.noreply.gitee.com>

--- physicsLab/__init__.py
+++ physicsLab/__init__.py
@@ -4,7 +4,7 @@ from physicsLab.experiment import *
 # 电学实验
 from physicsLab.electricity import *
 # 模块化电路
-from physicsLab.unionElectricity import *
+from physicsLab.unionElements import *
 # 天体物理实验
 from physicsLab.astrophysics import *
 # 电与磁实验
