0d3e29cfe03066b30d77781c643e4bee554a44c6
Goodenough <11533108+script2000@user.noreply.gitee.com>
2023-02-19 06:30:13 +0000

    模块化电路
    Signed-off-by: Goodenough <11533108+script2000@user.noreply.gitee.com>

--- physicsLab/unionElements/union.py
+++ physicsLab/unionElements/union.py
@@ -16,14 +16,14 @@ class union_Sum:
         for count in range(1, bitCount):
             if count % 8 != 0:
                 y = myRound(y + 0.2)
-                crt_wire(
+                crt_Wire(
                     Full_Adder(x, y, z).i_low,
                     get_Element(x, y - 0.2, z).o_low
                 )
             else:
                 y -= 1.4
                 z = myRound(z + 0.1)
-                crt_wire(
+                crt_Wire(
                     Full_Adder(x, y, z).i_low,
                     get_Element(x, y + 1.4, z - 0.1).o_low
                 )
@@ -44,8 +44,8 @@ class union_2_4_Decoder:
         obj2 = Nimp_Gate(x, y + 0.1, z)
         obj3 = Nimp_Gate(x, y + 0.2, z)
         obj4 = And_Gate(x, y + 0.3, z)
-        crt_wire(obj1.i_up, obj2.i_low), crt_wire(obj2.i_low, obj3.i_up), crt_wire(obj3.i_up, obj4.i_up)
-        crt_wire(obj1.i_low, obj2.i_up), crt_wire(obj2.i_up, obj3.i_low), crt_wire(obj3.i_low, obj4.i_low)
+        crt_Wire(obj1.i_up, obj2.i_low), crt_Wire(obj2.i_low, obj3.i_up), crt_Wire(obj3.i_up, obj4.i_up)
+        crt_Wire(obj1.i_low, obj2.i_up), crt_Wire(obj2.i_up, obj3.i_low), crt_Wire(obj3.i_low, obj4.i_low)
 
     @property
     def i_up(self):
@@ -67,21 +67,21 @@ class union_4_16_Decoder:
         obj3 = Nimp_Gate(x, y + 0.2, z); obj4 = And_Gate(x, y + 0.3, z)
         obj5 = Nor_Gate(x + 0.15, y, z); obj6 = Nimp_Gate(x + 0.15, y + 0.1, z)
         obj7 = Nimp_Gate(x + 0.15, y + 0.2, z); obj8 = And_Gate(x + 0.15, y + 0.3, z)
-        crt_wire(obj1.i_up, obj2.i_low), crt_wire(obj2.i_low, obj3.i_up), crt_wire(obj3.i_up, obj4.i_up)
-        crt_wire(obj5.i_up, obj6.i_low), crt_wire(obj6.i_low, obj7.i_up), crt_wire(obj7.i_up, obj8.i_up)
-        crt_wire(obj1.i_low, obj2.i_up), crt_wire(obj2.i_up, obj3.i_low), crt_wire(obj3.i_low, obj4.i_low)
-        crt_wire(obj5.i_low, obj6.i_up), crt_wire(obj6.i_up, obj7.i_low), crt_wire(obj7.i_low, obj8.i_low)
+        crt_Wire(obj1.i_up, obj2.i_low), crt_Wire(obj2.i_low, obj3.i_up), crt_Wire(obj3.i_up, obj4.i_up)
+        crt_Wire(obj5.i_up, obj6.i_low), crt_Wire(obj6.i_low, obj7.i_up), crt_Wire(obj7.i_up, obj8.i_up)
+        crt_Wire(obj1.i_low, obj2.i_up), crt_Wire(obj2.i_up, obj3.i_low), crt_Wire(obj3.i_low, obj4.i_low)
+        crt_Wire(obj5.i_low, obj6.i_up), crt_Wire(obj6.i_up, obj7.i_low), crt_Wire(obj7.i_low, obj8.i_low)
         for i in [0.3, 0.45, 0.6, 0.75]:
             for j in [0.05, 0.25]:
                 obj = Multiplier(x + i, y + j, z)
                 if j == 0.05:
-                    crt_wire(obj1.o, obj.i_low)
-                    crt_wire(obj2.o, obj.i_up)
+                    crt_Wire(obj1.o, obj.i_low)
+                    crt_Wire(obj2.o, obj.i_up)
                 else:
-                    crt_wire(obj3.o, obj.i_low)
-                    crt_wire(obj4.o, obj.i_up)
-                eval(f'crt_wire(obj{round((i - 0.3) / 0.15) + 5}.o, obj.i_upmid)')
-                eval(f'crt_wire(obj{round((i - 0.3) / 0.15) + 5}.o, obj.i_lowmid)')
+                    crt_Wire(obj3.o, obj.i_low)
+                    crt_Wire(obj4.o, obj.i_up)
+                eval(f'crt_Wire(obj{round((i - 0.3) / 0.15) + 5}.o, obj.i_upmid)')
+                eval(f'crt_Wire(obj{round((i - 0.3) / 0.15) + 5}.o, obj.i_lowmid)')
 
     @property
     def i_up(self):
