e8e43793be6a0aa889bc9fe8d3e6fbb5b1563b1a
Goodenough <2381642961@qq.com>
2023-03-16 23:06:28 +0800

    修复了文件import的问题

--- experiment.py
+++ experiment.py
@@ -5,8 +5,8 @@ import random as _random
 import string as _string
 import os as _os
 
-from _utf_8 import utf8_coding as _utf8_coding
-import _fileGlobals
+from physicsLab._utf_8 import utf8_coding as _utf8_coding
+import physicsLab._fileGlobals as _fileGlobals
 from electricity.element import crt_Element
 
 ### define ###
