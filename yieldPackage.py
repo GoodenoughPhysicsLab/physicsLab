2f1f19d5af13d82cd4111d171e5024f2e382031d
Goodenough <2381642961@qq.com>
2023-04-14 23:06:53 +0800

    修复了read_Experiment()读取存档名的问题，同时现在支持多次打开存档，但只支持对最后一次打开的存档进行操作

--- yieldPackage.py
+++ yieldPackage.py
@@ -3,19 +3,19 @@ import os
 import git
 
 if __name__ == '__main__':
-    string = input('update setup.py?(y/n)')
 
-    if string:
+    string = input('upgrade package to pypi?(y/n)')
+    if string == 'y':
         os.system('D:')
-        os.system('cd D:\\program physicsLab\\hub')
+        os.system('cd D:\\program physicsLab')
         os.system('py setup.py sdist bdist_wheel')
         # os.system('twine upload hub/dist/*')
         # 更新python包
         # pip install --upgrade xxx
 
-    string = input('push?(y/n)')
+    string = input('push file to hub?(y/n)')
     if string == 'y':
-        repo = git.Git("/hub")
+        repo = git.Git("D:\\program physicsLab")
         repo.checkout('master')
         repo.merge('dev')
         repo.push('gitee', 'master')
