622af6560b5ec5da3b87b18c9abb0e78f7bee953
Goodenough <2381642961@qq.com>
2023-04-21 23:10:06 +0800

    更新了发布程序

--- yieldPackage.py
+++ yieldPackage.py
@@ -5,10 +5,10 @@ import git
 if __name__ == '__main__':
     string = input('upgrade package to pypi?(y/n)')
     if string == 'y':
+        os.system('cd D:\\program_physicsLab')
         os.system('D:')
-        os.system('cd D:\\program physicsLab')
         os.system('py setup.py sdist bdist_wheel')
-        # os.system('twine upload hub/dist/*')
+        # os.system('twine upload dist/*')
         # 更新python包
         # pip install --upgrade xxx
 
