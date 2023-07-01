7041df214f704646ffb4f28cca424fe535289315
Goodenough <2381642961@qq.com>
2023-07-01 12:16:37 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("D:/program_physicsLab/README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="physicsLab",  # 用自己的名替换其中的YOUR_USERNAME_
-    version="1.2.2",  # 包版本号，便于维护版本
+    version="1.3.0",  # 包版本号，便于维护版本
     author="Goodenough",  # 作者，可以写自己的姓名
     author_email="2381642961@qq.com",  # 作者联系方式，可写自己的邮箱地址
     description="Doing experiments in the physics lab AR by python",  # 包的简述
