631e6b7a549bb002eae6b6dda981ef1bffa788aa
Goodenough <2381642961@qq.com>
2023-11-12 23:43:05 +0800

    update make.bat: auto serch of *.pyd

--- plmidiModule/make.bat
+++ plmidiModule/make.bat
@@ -1,6 +1,12 @@
-@REM 删除旧的build
+@REM 切换至plmidiModule目录运行该批处理程序
+
+@echo off
+
 rd /s /q .\build
-@REM 注释：运行该批处理文件必须在plmidiMoudule文件夹下
-py plmidi_setup.py build
-@REM 将.pyd复制到venv以便于测试
-copy  .\build\lib.win-amd64-3.8\*.pyd  ..\venv\Lib\site-packages
+
+..\venv\Scripts\python.exe plmidi_setup.py build
+
+rem 将*.pyd复制到venv目录下以使venv中的python能够使用
+for /d /r "build" %%d in (lib.*) do (
+    copy %%d\*.pyd ..\venv\Lib\site-packages
+)
