8ad28d531b9e70ef4b74cc2294abe6bfab4b1a0c
Goodenough <2381642961@qq.com>
2024-01-06 20:39:44 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-import setuptools  # 导入setuptools打包工具
+import setuptools
 
 with open("./README_zh.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="physicsLab",
-    version="1.4.2",  # 包版本号，便于维护版本
+    version="1.4.2",
     license="MIT",
-    author="Goodenough",  # 作者，可以写自己的姓名
-    author_email="2381642961@qq.com",  # 作者联系方式，可写自己的邮箱地址
-    description="python API for physics-lab-AR",  # 包的简述
-    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    author="Goodenough",
+    author_email="2381642961@qq.com",
+    description="Python API for physics-lab-AR",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitee.com/script2000/physicsLab",  # 项目地址
+    url="https://gitee.com/script2000/physicsLab",
     packages=setuptools.find_packages(),
     install_requires=["mido"],
     classifiers=[
@@ -22,5 +22,5 @@ setuptools.setup(
         "Natural Language :: Chinese (Simplified)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7'  # 对python的最低版本要求
+    python_requires='>=3.7',
 )
