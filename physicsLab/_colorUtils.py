87171113a8e49440a72302dd4414e827c5b13039
Goodenough <2381642961@qq.com>
2024-06-14 17:39:41 +0800

    fix color support on win7

--- physicsLab/_colorUtils.py
+++ physicsLab/_colorUtils.py
@@ -1,47 +1,36 @@
 # -*- coding: utf-8 -*-
-import platform
 from enum import Enum, unique
+
+import colorama
+colorama.init(autoreset=True)
 
 @unique
 class COLOR(Enum):
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    _DEFAULT = '\033[39m'
+    BLACK = colorama.Fore.BLACK
+    RED = colorama.Fore.RED
+    GREEN = colorama.Fore.GREEN
+    YELLOW = colorama.Fore.YELLOW
+    BLUE = colorama.Fore.BLUE
+    MAGENTA = colorama.Fore.MAGENTA
+    CYAN = colorama.Fore.CYAN
+    WHITE = colorama.Fore.WHITE
 
 # 打印write_Experiment的信息时是否使用彩色字
-colorSupport = True
-
-if colorSupport and platform.system == "Windows":
-    try:
-        # https://stackoverflow.com/questions/36760127/...
-        # how-to-use-the-new-support-for-ansi-escape-sequences-in-the-windows-10-console
-        from ctypes import windll
-        kernel32 = windll.kernel32
-        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
-    except ImportError:
-        colorSupport = False
+_ColorSupport = True
 
 # 打印颜色字
 def color_print(msg: str, color: COLOR) -> None:
-    if not isinstance(color, COLOR):
+    if not isinstance(color, COLOR) or not isinstance(msg, str):
         raise TypeError
 
-    global colorSupport
+    global _ColorSupport
 
-    if not isinstance(msg, str):
-        raise TypeError
-    if colorSupport:
-        print(f"{color.value}{msg}{COLOR._DEFAULT.value}")
+    if _ColorSupport:
+        print(color.value + msg)
     else:
         print(msg)
 
 # 关闭打印时的color
 def close_color_print():
-    global colorSupport
-    colorSupport = False
+    global _ColorSupport
+    _ColorSupport = False
