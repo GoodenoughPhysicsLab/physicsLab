f7d007a1cda611c5ae59a79d5f338fe1bcd519b2
wsxiaolin <xiegushi2022@outlook.com>
2024-10-04 17:00:38 +0800

    .

--- physicsLab/web/webutils.py
+++ physicsLab/web/webutils.py
@@ -243,3 +243,104 @@ def get_avatars(search_id: str,
                     yield task.result()
                 except IndexError:
                     pass
+
+class Bot:
+    ''' 初始化 Bot 类的实例
+
+        @param username: 机器人的用户名
+        @param password: 机器人的密码
+        @param process_fn: 处理函数，用于处理捕获到的消息
+        @param catched: 当捕获到新消息时调用的函数
+        @param replyed: 当回复消息时调用的函数
+        @param finnished: 当所有消息处理完成时调用的函数
+    '''
+    def __init__(self,
+                user: api.User,
+                process_fn: Callable,
+                catched: Callable,
+                replyed: Callable,
+                finnished: Callable,
+                ) -> None:
+        ''' @param user: 机器人要绑定的用户账号
+        '''
+        if not isinstance(user, api.User):
+            raise TypeError
+
+        # 生命周期 捕获－处理－回复－记录[－完成]
+        self.user = user
+        # self.username = username
+        # self.password = password
+        self.process_fn = process_fn
+        self.catched = catched
+        self.replyed = replyed
+        self.pending = set()
+        self.finnish = set()
+        self.finnished = finnished
+        self.reply_config = {
+            "ignoreReplyToOters": True, # 如果出现回复@{非Bot的用户}，则忽略
+            "readHistory": True, # 捕获Bot启动前的消息（最多20条）
+            "replyRequired": True, # 只捕获回复@{Bot}的消息
+        }
+
+    ''' 初始化
+
+        @param ID: (str) 序列号
+        @param type: (str) Discusion 或 Experiment
+        @param reply_config: (dict, 可选) 回复配置，包含不同的选项来控制机器人行为。
+            - ignoreReplyToOters: (bool) 是否忽略对其他用户的回复。
+            - readHistory: (bool) 是否读取历史消息。
+            - replyRequired: (bool) 是否需要回复用户的消息。
+    '''
+    def setConfig(self, ID: str, type: str, reply_config=None) -> None:
+        if reply_config is None:
+            reply_config = {}
+
+        # super().__login(self, self.username, self.password)
+        self.bot_id = self.user.user_id
+        self.target_id = ID
+        self.target_type = type
+
+        # 合并默认配置和传入配置
+        self.reply_config.update(reply_config)
+
+        data = self.user.get_comments(self.target_id, self.target_type, 20)  # 假设 messages 是一个已定义的属性
+        if self.reply_config['readHistory']:
+            index = ""
+            msglist = data['Data']['Comments'][::-1]  # 试图反转列表
+            for comment in msglist:
+                if comment['UserID'] == self.bot_id:
+                    index = comment['ID']
+            self.start_index = index
+        else:
+            self.start_index = data['Data']['Comments'][0]['ID'] if data['Data']['Comments'] else ""
+
+    def run(self):
+        re = self.user.get_comments(self.target_id, self.target_type, 20)
+        for comment in re['Data']['Comments']:
+            if comment['ID'] == self.start_index:
+                break
+            if comment['UserID'] == self.bot_id:
+                continue
+            if comment['ID'] in self.pending or comment['ID'] in self.finnish:
+                continue
+            if ("回复<" in comment['Content'] and
+                self.reply_config['ignoreReplyToOters'] and
+                self.bot_id not in comment['Content']):
+                continue
+            if (self.bot_id not in comment['Content'] and
+                self.reply_config['replyRequired']):
+                continue
+
+            self.catched(comment)
+            self.pending.add(comment['ID'])
+            reply = self.process_fn(comment, self)
+            if reply == "":
+                continue
+
+            msg = f"回复@{comment['Nickname']}: {reply}"
+            self.user.post_comment(self.target_id, msg, self.target_type)
+            self.finnish.add(comment['ID'])
+            self.pending.remove(comment['ID'])
+            if not self.pending:
+                self.finnished(self.finnish)
+            self.replyed({**{"msg": msg}, **comment})
