75b59fea4a9dbb7fa44fd6ee36bad7499ca95585
Arendelle <2381642961@qq.com>
2024-11-13 22:24:25 +0800

    now, ThreadsManager is a thread pool

--- physicsLab/web/_threads_manager.py
+++ physicsLab/web/_threads_manager.py
@@ -7,44 +7,74 @@
     That's why I write this module.
 '''
 import os
+import time
+import queue
 import threading
 from physicsLab.typehint import Optional, Callable, Set, Self
 
-class _Task(threading.Thread):
-    def __init__(self, func: Callable, *args, **kwargs) -> None:
+class _Task:
+    class _WaitingResult:
+        def __new__(cls):
+            return cls
+
+    def __init__(self, func: Callable, *args, **kwargs):
         self.func = func
         self.args = args
         self.kwargs = kwargs
-        super().__init__(target=func, args=args, kwargs=kwargs)
-        self.daemon = True
+        self.func_res = self._WaitingResult
 
     def run(self):
         self.func_res = self.func(*self.args, **self.kwargs)
 
     def result(self):
-        while self.is_alive():
-            self.join(timeout=2)
+        while self.func_res is self._WaitingResult:
+            pass
         return self.func_res
 
-class ThreadsManager: # TODO: makes it a thread-pool
-    def __init__(self, max_workers: Optional[int] = None) -> None:
-        assert isinstance(max_workers, int) or max_workers is None
+def _office(tasks: queue.SimpleQueue):
+    ''' workers work here '''
+    while True:
+        while tasks.empty():
+            time.sleep(0.1)
+        task = tasks.get_nowait()
+        if task is _EndOfQueue:
+            tasks.put_nowait(_EndOfQueue)
+            break
+        task.run()
 
-        if max_workers is not None and max_workers < 1:
-            raise ValueError("max_workers must be greater than 0")
-        elif max_workers is None:
+class _EndOfQueue:
+    def __new__(cls):
+        return cls
+
+class ThreadsManager:
+    def __init__(self, max_workers: Optional[int] = None) -> None:
+        assert isinstance(max_workers, int) and max_workers > 0 \
+            or max_workers is None
+
+        if max_workers is None:
             max_workers = min(32, (os.cpu_count() or 1) + 4)
         self.max_workers = max_workers
+        self.workers: Set[threading.Thread] = set()
+        self.tasks = queue.SimpleQueue()
 
     def __enter__(self) -> Self:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        pass
+        for worker in self.workers:
+            while worker.is_alive():
+                worker.join(timeout=1)
 
     def submit(self, func: Callable, *args, **kwargs) -> _Task:
         assert callable(func)
 
-        thread = _Task(func, *args, **kwargs)
-        thread.start()
-        return thread
+        task = _Task(func, *args, **kwargs)
+        self.tasks.put_nowait(task)
+        if len(self.workers) < self.max_workers:
+            worker = threading.Thread(target=_office, args=(self.tasks,), daemon=True)
+            self.workers.add(worker)
+            worker.start()
+        return task
+
+    def submit_end(self) -> None:
+        self.tasks.put(_EndOfQueue)
