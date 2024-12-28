4b0ef5b81d1e7689541989cb41af8cca815381a0
Arendelle <2381642961@qq.com>
2024-12-28 18:42:07 +0800

    physicsLab v1.6.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="physicsLab",
-    version="1.6.2",
+    version="1.6.3",
     license="MIT",
     author="Arendelle",
     author_email="2381642961@qq.com",
