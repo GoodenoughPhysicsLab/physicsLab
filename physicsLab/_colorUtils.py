2e102ee26b6cdb947d92160d671d7b2fa317fbe2
Arendelle <2381642961@qq.com>
2025-03-10 10:26:05 +0800

    refactor colorUtils
    * bugfix cased by windows locale
    * drop colorama

--- physicsLab/_colorUtils.py
+++ physicsLab/_colorUtils.py
@@ -3,25 +3,36 @@
 import platform
 from enum import Enum, unique
 
-import colorama
-colorama.init(autoreset=True)
+# import colorama
+# colorama.init(autoreset=True)
 
 if platform.system() == "Windows":
-    import ctypes
+    import io
+    import sys
     # 设置终端的编码为UTF-8
-    kernel32 = ctypes.windll.kernel32
-    kernel32.SetConsoleOutputCP(65001)
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+# @unique
+# class COLOR(Enum):
+#     BLACK = colorama.Fore.BLACK
+#     RED = colorama.Fore.RED
+#     GREEN = colorama.Fore.GREEN
+#     YELLOW = colorama.Fore.YELLOW
+#     BLUE = colorama.Fore.BLUE
+#     MAGENTA = colorama.Fore.MAGENTA
+#     CYAN = colorama.Fore.CYAN
+#     WHITE = colorama.Fore.WHITE
 
 @unique
 class COLOR(Enum):
-    BLACK = colorama.Fore.BLACK
-    RED = colorama.Fore.RED
-    GREEN = colorama.Fore.GREEN
-    YELLOW = colorama.Fore.YELLOW
-    BLUE = colorama.Fore.BLUE
-    MAGENTA = colorama.Fore.MAGENTA
-    CYAN = colorama.Fore.CYAN
-    WHITE = colorama.Fore.WHITE
+    BLACK = 0
+    RED = 1
+    GREEN = 2
+    YELLOW = 3
+    BLUE = 4
+    MAGENTA = 5
+    CYAN = 6
+    WHITE = 7
 
 _ColorSupport = True
 
@@ -31,10 +42,12 @@ def color_print(msg: str, color: COLOR, end='\n') -> None:
 
     global _ColorSupport
 
-    if _ColorSupport:
-        print(color.value + msg, end=end)
-    else:
-        print(msg, end=end)
+    # if _ColorSupport:
+    #     print(color.value + msg, end=end)
+    # else:
+    #     print(msg, end=end)
+    print(msg, end=end)
+    # print(msg.encode("utf-8").decode("utf-8"), end=end)
 
 def close_color_print():
     ''' 关闭打印的文字是有颜色的功能 '''
