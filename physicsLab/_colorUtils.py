fd9e53768a57f2d3dbf102948fec2f5322f1452a
Arendelle <2381642961@qq.com>
2025-03-30 18:07:28 +0800

    Squashed commit of the following:
    commit 853d82ec3d7ff3c47ca7099bbccfe574ea76483b
    Author: Arendelle <2381642961@qq.com>
    Date:   Sat Mar 29 22:32:33 2025 +0800
    
        Fix print info of Experiment.delete
    
    commit 852450b4499188ef5cca01ac0f424b60e57b1adc
    Author: Arendelle <2381642961@qq.com>
    Date:   Sat Mar 29 22:29:23 2025 +0800
    
        Rename Grey to White
    
    commit 7574d5cd9ea20cbf04f2458438975221ab504514
    Author: Arendelle <2381642961@qq.com>
    Date:   Sat Mar 29 22:26:33 2025 +0800
    
        Drop colorama
    
        * fix bug caused by flush
    
    commit 2ca171214e173d580c87e664b5e5fc051ad7a6c1
    Author: Arendelle <2381642961@qq.com>
    Date:   Sat Mar 29 21:50:59 2025 +0800
    
        Bugfix of windows
    
    commit 080d8d12664af2412aca1217b1c242c03c80974e
    Author: Arendelle <2381642961@qq.com>
    Date:   Sat Mar 29 20:52:45 2025 +0800
    
        Drop colorama

--- physicsLab/_colorUtils.py
+++ physicsLab/_colorUtils.py
@@ -3,14 +3,53 @@
     为最基础, 最底层的设施
 '''
 
+import platform
+from physicsLab._typing import final
+
 # 设置终端的编码为UTF-8
 import io
 import sys
-sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
 
-import colorama
+if platform.system() == "Windows":
+    import ctypes
+    from ctypes import wintypes
+
+    class _CONSOLE_SCREEN_BUFFER_INFO(ctypes.Structure):
+        _fields_ = [
+            ("dwSize", wintypes._COORD),
+            ("dwCursorPosition", wintypes._COORD),
+            ("wAttributes", wintypes.WORD),
+            ("srWindow", wintypes.SMALL_RECT),
+            ("dwMaximumWindowSize", wintypes._COORD),
+        ]
+
+    kernel32 = ctypes.windll.kernel32
+
+    _GetStdHandle = ctypes.windll.kernel32.GetStdHandle
+    _GetStdHandle.argtypes = [
+        wintypes.DWORD,
+    ]
+    _GetStdHandle.restype = wintypes.HANDLE
+
+    _GetConsoleScreenBufferInfo = ctypes.windll.kernel32.GetConsoleScreenBufferInfo
+    _GetConsoleScreenBufferInfo.argtypes = [
+        wintypes.HANDLE,
+        ctypes.POINTER(_CONSOLE_SCREEN_BUFFER_INFO),
+    ]
+    _GetConsoleScreenBufferInfo.restype = wintypes.BOOL
+
+    _SetConsoleTextAttribute = ctypes.windll.kernel32.SetConsoleTextAttribute
+    _SetConsoleTextAttribute.argtypes = [
+        wintypes.HANDLE,
+        wintypes.WORD,
+    ]
+    _SetConsoleTextAttribute.restype = wintypes.BOOL
+
+    _stdout_handle = _GetStdHandle(-11) # STD_OUTPUT_HANDLE
+    _stderr_handle = _GetStdHandle(-12)
 
 _color_support = True
 
@@ -19,67 +58,105 @@ def close_color_print():
     global _color_support
     _color_support = False
 
-class Color:
+class _Color:
+    fore: int
+
     def __init__(self, msg: str) -> None:
         self.msg = msg
 
-class Black(Color):
+    @final
+    def cprint(self):
+        if platform.system() == "Windows":
+            # 先刷新再打印, 避免以下的bug
+            # print("test")
+            # _colorUtils.cprint("test")
+            sys.stdout.flush()
+            csbi = _CONSOLE_SCREEN_BUFFER_INFO()
+            _GetConsoleScreenBufferInfo(_stdout_handle, ctypes.byref(csbi))
+            _SetConsoleTextAttribute(_stdout_handle, self.fore)
+            print(self.msg, flush=True, end='')
+            _SetConsoleTextAttribute(_stdout_handle, csbi.wAttributes)
+        else:
+            print(f"\033[{self.fore}m{self.msg}\033[39m", end='')
+
+class Black(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Red(Color):
+        if platform.system() == "Windows":
+            self.fore = 0
+        else:
+            self.fore = 30
+
+class Red(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Green(Color):
+        if platform.system() == "Windows":
+            self.fore = 4
+        else:
+            self.fore = 31
+
+class Green(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Yellow(Color):
+        if platform.system() == "Windows":
+            self.fore = 2
+        else:
+            self.fore = 32
+
+class Yellow(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Blue(Color):
+        if platform.system() == "Windows":
+            self.fore = 6
+        else:
+            self.fore = 33
+
+
+class Blue(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Magenta(Color):
+        if platform.system() == "Windows":
+            self.fore = 1
+        else:
+            self.fore = 34
+
+class Magenta(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
 
-class Cyan(Color):
+        if platform.system() == "Windows":
+            self.fore = 5
+        else:
+            self.fore = 35
+
+class Cyan(_Color):
    def __init__(self, msg: str) -> None:
-       super().__init__(msg)
+        super().__init__(msg)
 
-class White(Color):
+        if platform.system() == "Windows":
+            self.fore = 3
+        else:
+            self.fore = 36
+
+class White(_Color):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
+
+        if platform.system() == "Windows":
+            self.fore = 7
+        else:
+            self.fore = 37
 
 def cprint(*args, end='\n'):
     global _color_support
     for arg in args:
-        if _color_support:
-            if isinstance(arg, Black):
-                print(colorama.Fore.BLACK + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Red):
-                print(colorama.Fore.RED + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Green):
-                print(colorama.Fore.GREEN + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Yellow):
-                print(colorama.Fore.YELLOW + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Blue):
-                print(colorama.Fore.BLUE + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Magenta):
-                print(colorama.Fore.MAGENTA + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, Cyan):
-                print(colorama.Fore.CYAN + arg.msg + colorama.Fore.RESET, end='')
-            elif isinstance(arg, White):
-                print(colorama.Fore.WHITE + arg.msg + colorama.Fore.RESET, end='')
-            else:
-                print(arg, end='')
+        if _color_support and isinstance(arg, _Color):
+            arg.cprint()
         else:
-            if isinstance(arg, Color):
-                print(arg.msg, end='')
-            else:
-                print(arg, end='')
+            print(arg, end='')
     print(end, end='')
