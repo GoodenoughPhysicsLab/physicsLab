e63ec16fb962d9c46e44f60fffca8da759790b76
Arendelle <121805642+GoodenoughPhysicsLab@users.noreply.github.com>
2025-04-10 18:22:37 +0800

    Unrecoverable Error (#69)
    * Add _unrecoverable_error
    
    * assertion_error
    * type_error
    
    * Small improve of Exceptions defined in errors.py
    
    * Complete getting core source code
    
    * Small imporve of type_error
    
    * Complete support for sync-function
    
    * Fix print bugs by flush stdout by band
    
    * Complete type_error
    
    * Improve assertion_error

--- physicsLab/errors.py
+++ physicsLab/errors.py
@@ -1,43 +1,158 @@
 # -*- coding: utf-8 -*-
+''' physicsLab的异常系统
+    有2个主要的组成部分: 可恢复的异常, 不可恢复的错误
+
+    * 可恢复的异常: 基于Python的Exception自定义的一系列错误类
+
+    * 不可恢复的错误:
+        当某些错误发生的时候, physicsLab认为程序抽象机已经崩溃, 无法继续运行
+        因此一旦这些错误发生, physicsLab会调用os.abort来终止程序, 而不是抛出一个异常
+        因为抽象机崩溃的情况下, 程序已经无法继续运行, 最需要的是修bug, 因此坚决不给用户捕获异常的可能
+        被视为 不可恢复的错误 的有:
+        * assertion_error: 断言错误, physicsLab认为其为不可恢复的错误, 因此请不要使用 AssertionError
+        * type_error: 断言错误, physicsLab认为其为不可恢复的错误, 因此请不要使用 TypeError
+        除此之外, physicsLab自定义了不可恢复错误发生时的打印输出格式
+        这些格式比Python的traceback可读性更好
+'''
+
+import os
+import sys
+import ast
+import math
+import inspect
+import threading
+import executing
+
 from ._typing import NoReturn
-from physicsLab import unwind
+from physicsLab import _unwind
 from physicsLab import _colorUtils
-from physicsLab._typing import Optional
+from physicsLab._typing import Optional, LiteralString
 
 BUG_REPORT: str = "please send a bug-report at " \
                 "https://github.com/GoodenoughPhysicsLab/physicsLab/issues or " \
                 "https://gitee.com/script2000/physicsLab/issues " \
-                "with your code, *.sav and traceback"
+                "with your code, *.sav and traceback / coredump for Python"
 
-def unrecoverable_error(err_type: str, msg: Optional[str]) -> NoReturn:
-    assert isinstance(err_type, str) and isinstance(msg, str), BUG_REPORT
-    unwind.print_stack()
-    _colorUtils.cprint(_colorUtils.Red(err_type), end='')
+def _unrecoverable_error(err_type: str, msg: Optional[str]) -> NoReturn:
+    ''' 不可恢复的错误, 表明程序抽象机已崩溃
+        会打印的错误信息并退出程序
+    '''
+    _colorUtils.cprint(_colorUtils.Red(err_type), end='', file=sys.stderr)
     if msg is None:
-        print('\n')
+        print('\n', file=sys.stderr)
     else:
-        _colorUtils.cprint(": ", _colorUtils.Red(msg))
-    exit(1)
+        _colorUtils.cprint(": ", _colorUtils.Red(msg), file=sys.stderr)
+    sys.stdout.flush()
+    sys.stderr.flush()
+    os.abort()
+
+_unrecoverable_error_lock = threading.Lock()
 
 def assertion_error(msg: str) -> NoReturn:
-    unrecoverable_error("AssertionError", msg)
-
-def type_error(msg: Optional[str] = None) -> NoReturn:
-    unrecoverable_error("TypeError", msg)
+    ''' 断言错误, physicsLab认为其为不可恢复的错误
+    '''
+    _unrecoverable_error_lock.acquire()
+    _unwind.print_stack(full=True)
+    _unrecoverable_error("AssertionError", msg)
 
 def assert_true(
         condition: bool,
         msg: str = BUG_REPORT,
 ) -> None:
     if not condition:
-        raise assertion_error(msg)
+        assertion_error(msg)
 
 def unreachable() -> NoReturn:
     assertion_error(f"Unreachable touched, {BUG_REPORT}")
 
+def type_error(msg: Optional[str] = None) -> NoReturn:
+    ''' 类型错误, physicsLab认为其为不可恢复的错误
+    '''
+    _unrecoverable_error_lock.acquire()
+    current_frame = inspect.currentframe()
+    if current_frame is None:
+        unreachable()
+
+    declare_frame = current_frame.f_back
+    if declare_frame is None:
+        unreachable()
+    declare_node = executing.Source.executing(declare_frame).node
+    declare_module = inspect.getmodule(declare_frame)
+    if declare_module is None:
+        unreachable()
+
+    call_frame = declare_frame.f_back
+    if call_frame is None:
+        unreachable()
+    call_module = inspect.getmodule(call_frame)
+    call_executing = executing.Source.executing(call_frame)
+    call_node = call_executing.node
+    if call_node is None:
+        # executing 的原理是通过解析字节码`__code__`来获取对应的节点
+        # 但executing支持的字节码有限, 比如await有关的字节码就不支持, 无法获取对应的node
+        # 此时只好用inspect.stack来反射获取相对原始的源代码信息
+        call_frame_info = inspect.stack()[2]
+        if call_frame_info is None or call_frame_info.code_context is None:
+            unreachable()
+        _unwind.print_code_block(
+            lambda: _colorUtils.cprint(
+                " File ",
+                _colorUtils.Magenta(f"\"{call_frame.f_code.co_filename}\""),
+                ", in ",
+                _colorUtils.Magenta(call_executing.code_qualname()),
+                file=sys.stderr
+            ),
+            call_frame_info.lineno,
+            call_frame_info.code_context[0].strip(),
+        )
+    else:
+        if call_module is None:
+            unreachable()
+        call_src = ast.get_source_segment(inspect.getsource(call_module), call_node, padded=True)
+        if call_src is None:
+            unreachable()
+        _unwind.print_code_block(
+            lambda: _colorUtils.cprint(
+                " File ",
+                _colorUtils.Magenta(f"\"{call_frame.f_code.co_filename}\""),
+                ", in ",
+                _colorUtils.Magenta(call_executing.code_qualname()),
+                file=sys.stderr
+            ),
+            call_frame.f_lineno,
+            call_src,
+        )
+
+        while not isinstance(declare_node, (ast.FunctionDef, ast.AsyncFunctionDef)):
+            declare_node = declare_node.parent
+        func_declare = ast.get_source_segment(inspect.getsource(declare_module), declare_node, padded=True)
+        if func_declare is None:
+            unreachable()
+        is_signature = 0
+        declare_output: str = ""
+        for char in func_declare:
+            if char == '(':
+                is_signature += 1
+            elif char == ')':
+                is_signature -= 1
+            elif char == ':' and is_signature == 0:
+                declare_output += '\n'
+                break
+            declare_output += char
+
+        _unwind.print_code_block(
+            lambda: _colorUtils.cprint(
+                _colorUtils.Yellow(" Note"), ": function defined here:",
+                file=sys.stderr,
+            ),
+            declare_node.lineno,
+            declare_output,
+        )
+
+    _unrecoverable_error("TypeError", msg)
+
 class InvalidWireError(Exception):
     def __init__(self, msg: str):
-        assert isinstance(msg, str)
         self.msg = msg
 
     def __str__(self):
@@ -66,11 +181,9 @@ class ExperimentExistError(Exception):
 
 class ExperimentNotExistError(Exception):
     ''' 实验不存在 '''
-    err_msg = "The experiment does not exist"
+    def __init__(self, err_msg: LiteralString = "The experiment does not exist") -> None:
+        self.err_msg = err_msg
 
-    def __init__(self, err_msg = None) -> None:
-        if err_msg is not None:
-            self.err_msg = err_msg
     def __str__(self):
         return self.err_msg
 
@@ -86,17 +199,17 @@ class ExperimentHasNotCrtError(Exception):
 
 class ExperimentTypeError(Exception):
     ''' 打开的实验与调用的元件不符 '''
-    def __init__(self, err_msg: str = "The type of experiment does not match the element"):
+    def __init__(self, err_msg: LiteralString = "The type of experiment does not match the element") -> None:
         self.err_msg = err_msg
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.err_msg
 
 # 用于get_Element 获取元件引用失败
 class ElementNotFound(Exception):
-    def __init__(self, err_msg: str = "") -> None:
-        self.err_msg: str = err_msg
-    def __str__(self):
+    def __init__(self, err_msg: LiteralString = "Can't find element") -> None:
+        self.err_msg = err_msg
+    def __str__(self) -> str:
         return self.err_msg
 
 class ExperimentError(Exception):
@@ -108,15 +221,17 @@ class ExperimentError(Exception):
 
 class ResponseFail(Exception):
     ''' 返回消息体失败 '''
-    def __init__(self, err_msg: str):
+    # TODO 获取对应的错误码
+    def __init__(self, err_msg: LiteralString):
         self.err_msg: str = err_msg
+
     def __str__(self):
         return self.err_msg
 
 class MaxRetryError(Exception):
     ''' 重试次数过多 '''
-    def __init__(self, err_msg: str):
-        self.err_msg: str = err_msg
+    def __init__(self, err_msg: LiteralString):
+        self.err_msg = err_msg
 
     def __str__(self):
         return self.err_msg
